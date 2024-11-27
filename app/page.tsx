7d4ec113660e2e3a7fa1d870b24c8da32a6aea80
alokranjan04 <alokranjan04@gmail.com>
2024-11-28 01:42:37 +0530

    deploy issue fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { Settings } from "lucide-react";
+import { ModeToggle } from "@/components/modetoggle";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import ReportComponent from "@/components/ReportComponent";
+import ChatComponent from "@/components/chatcomponent";
 
-const HomeComponent = (props: Props) => {
+const Home: React.FC = () => {
+  const { toast } = useToast();
+
+  const [reportData, setreportData] = useState<string>("");
+
+  const onReportConfirmation = (data: string): void => {
+    setreportData(data);
+    toast({
+      description: "Updated!",
+    });
+  };
+
   return (
-    <div>HomeComponent</div>
-  )
-}
+    <div className="grid h-screen w-full">
+      <div className="flex flex-col">
+        <header className="sticky top-0 z-10 flex h-[57px] bg-background items-center gap-1 border-b px-4">
+          <h1 className="text-xl font-semibold text-[#D90013]">
+            <span className="flex flex-row">CareExtract</span>
+          </h1>
+          <div className="w-full flex flex-row justify-end gap-2">
+            <ModeToggle />
+            <Drawer>
+              <DrawerTrigger asChild>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                  <Settings />
+                  <span className="sr-only">Settings</span>
+                </Button>
+              </DrawerTrigger>
+              <DrawerContent className="max-h-[80vh]">
+                <ReportComponent onReportConfirmation={onReportConfirmation} />
+              </DrawerContent>
+            </Drawer>
+          </div>
+        </header>
+        <main
+          className="grid flex-1 gap-4 overflow-auto p-4
+          md:grid-cols-2
+          lg:grid-cols-3"
+        >
+          <div className="hidden md:flex flex-col">
+            <ReportComponent onReportConfirmation={onReportConfirmation} />
+          </div>
+          <div className="lg:col-span-2">
+            <ChatComponent reportData={reportData} />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
 
-export default HomeComponent
+export default Home;
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // Your Next.js configuration options here
+  reactStrictMode: true,
+  // ...other configurations
+};
